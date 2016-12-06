84716cc06793cb85d4124eadce312121c130df1c
dankito <dankito@user.noreply.github.com>
2016-12-06 23:40:09 +0100

    Removed FloatingActionButton from MainActivity

--- QRCodeAndroid/src/main/java/net/dankito/qrcode/MainActivity.java
+++ QRCodeAndroid/src/main/java/net/dankito/qrcode/MainActivity.java
@@ -2,15 +2,12 @@ package net.dankito.qrcode;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import net.dankito.qrcode.adapter.MainActivityTabsAdapter;
 import net.dankito.qrcode.fragments.ReadBarcodeFragment;
@@ -35,15 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
     TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
     tabLayout.setupWithViewPager(viewPager);
-
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-      }
-    });
 
   }
 
